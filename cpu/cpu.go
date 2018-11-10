f9fbd866a536d16e1cf77babb9fe3b9b1962d2d5
Yasushi Ichikawa <ichikaway@gmail.com>
2018-11-10 16:28:08 +0900

    コード整形

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Cpu struct {
-	CpuBus cpubus.CpuBus
+	CpuBus     cpubus.CpuBus
 	Interrupts cpu_interrupts.Interrupts
 }
 
 func NewCpu(cpubus cpubus.CpuBus, interrupts cpu_interrupts.Interrupts) Cpu {
 
-	return Cpu {
-		CpuBus: cpubus,
+	return Cpu{
+		CpuBus:     cpubus,
 		Interrupts: interrupts,
 	}
 }
