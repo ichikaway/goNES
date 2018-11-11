95d2752f514a23e61e64df9374c98ae6d7191d1d
Yasushi Ichikawa <ichikaway@gmail.com>
2018-11-11 19:14:07 +0900

    cpu構造体のレジスタ初期化処理

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,6 +1,8 @@
 package cpu
 
 import (
+	"fmt"
+	"goNES/cpu/registers"
 	"goNES/cpu_interrupts"
 	"goNES/cpubus"
 )
@@ -8,6 +10,8 @@ import (
 type Cpu struct {
 	CpuBus     cpubus.CpuBus
 	Interrupts cpu_interrupts.Interrupts
+	Registers  registers.Registers
+	HasBranched bool
 }
 
 func NewCpu(cpubus cpubus.CpuBus, interrupts cpu_interrupts.Interrupts) Cpu {
@@ -15,5 +19,8 @@ func NewCpu(cpubus cpubus.CpuBus, interrupts cpu_interrupts.Interrupts) Cpu {
 	return Cpu{
 		CpuBus:     cpubus,
 		Interrupts: interrupts,
+		Registers:  registers.GetDefaultRegisters(),
+		HasBranched: false,
+
 	}
 }
