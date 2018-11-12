bb2fa64eef9a09754b0473cdd2ac78766ec57558
Yasushi Ichikawa <ichikaway@gmail.com>
2018-11-13 08:28:57 +0900

    コード整形

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -8,20 +8,19 @@ import (
 )
 
 type Cpu struct {
-	CpuBus     cpubus.CpuBus
-	Interrupts cpu_interrupts.Interrupts
-	Registers  registers.Registers
+	CpuBus      cpubus.CpuBus
+	Interrupts  cpu_interrupts.Interrupts
+	Registers   registers.Registers
 	HasBranched bool
 }
 
 func NewCpu(cpubus cpubus.CpuBus, interrupts cpu_interrupts.Interrupts) Cpu {
 
 	return Cpu{
-		CpuBus:     cpubus,
-		Interrupts: interrupts,
-		Registers:  registers.GetDefaultRegisters(),
+		CpuBus:      cpubus,
+		Interrupts:  interrupts,
+		Registers:   registers.GetDefaultRegisters(),
 		HasBranched: false,
-
 	}
 }
 
