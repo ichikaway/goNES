ef628c83f4d79c8f62d4091de86c23b3ace0e5a9
Yasushi Ichikawa <ichikaway@gmail.com>
2019-01-04 14:14:41 +0900

    renderingData構造体を定義
    このデータを元にrender処理を行う

---
+++ ppu/renderData.go
@@ -0,0 +1,15 @@
+package ppu
+
+type RenderingData struct {
+	Palette    []byte
+	Background Background
+	Sprites    []SpriteWithAttribute
+}
+
+/** @var int[] */
+//public $palette;
+/** @var \Nes\Ppu\Tile[] */
+//public $background;
+/** @var \Nes\Ppu\SpriteWithAttribute[] */
+//public $sprites;
+
