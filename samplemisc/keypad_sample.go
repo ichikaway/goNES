171ee12e8059290c7ea6489a5529b264b389f3d0
Yasushi Ichikawa <ichikaway@gmail.com>
2019-04-09 09:01:56 +0900

    termboxを使ったコントローラ実装サンプル

--- samplemisc/keypad_sample.go
+++ samplemisc/keypad_sample.go
@@ -3,26 +3,71 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/nsf/termbox-go"
 	"os"
+	"sync"
 	"time"
 )
 
+var mu sync.Mutex
+
 func keyinput(key chan<- byte){
 	for{
+		in := bufio.NewReader(os.Stdin)
+		b, _ := in.ReadByte()
+		key <- b
+
+		/*
 		consoleReader := bufio.NewReaderSize(os.Stdin, 1)
 		input, _ := consoleReader.ReadByte()
 		//fmt.Println(input)
 		key <- input
+		*/
 	}
 
 }
 
+func keyEvent(kch chan termbox.Key) {
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			kch <- ev.Key
+		default:
+		}
+	}
+}
+
 func main() {
 	fmt.Println("keypad test")
-	key := make(chan byte)
 
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	keyCh := make(chan termbox.Key)
+	go keyEvent(keyCh)
+
+	for {
+		time.Sleep(50 * time.Millisecond)
+		select {
+			case key := <-keyCh:
+				mu.Lock()
+				switch key {
+				case termbox.KeyEsc, termbox.KeyCtrlC: //終了
+					mu.Unlock()
+					return
+				}
+				mu.Unlock()
+				fmt.Println(key)
+				break
+			default:
+				break
+		}
+	}
+
+/*	key := make(chan byte)
 	go keyinput(key)
-
 	for i:=0;i<1000;i++ {
 		//fmt.Print(" ")
 		select{
@@ -32,6 +77,6 @@ func main() {
 			time.Sleep(1000 * time.Millisecond)
 		}
 
-	}
+	}*/
 
 }
